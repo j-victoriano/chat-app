5eb7d79ad67a750409c476f3b45eaa659e251f68
Julian Victoriano <jvictoriano35@yahoo.com>
2023-09-26 22:17:29 -0700

    successfully added socket connection to server and client

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const socket = require('socket.io');
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
 
 require('./config/mongoose.config')
 
-app.listen(8000, () => console.log('Listening on Port 8000'))
+const server = app.listen(8000, () => console.log('Listening on Port 8000'))
+
+const io = socket(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['*'],
+        credentials: true,
+    }
+});
+
+io.on("connection", socket => {
+    console.log('socket id: ' + socket.id);
+
+    socket.on("event_from_client", data => {
+        socket.broadcast.emit("event_to_all_other_clients", data);
+    });
+});
+
