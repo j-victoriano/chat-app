6724b3b312e014c9db50f9c6bcdd166a90c326dd
Julian Victoriano <jvictoriano35@yahoo.com>
2023-09-27 12:46:01 -0700

    added authentication to user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,20 +1,45 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, "Username is required"],
         unique: true,
     },
     email: {
         type: String,
-        required: true,
+        required: [, "Email is required"],
         unique: true,
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+        }
     },
     password: {
         type: String,
-        required: true,
+        required: [true, "Password is required"],
+        minLength: [8, "Password must be at least 8 characters"]
     }
+}, { timestamps: true })
+
+UserSchema.virtual('confirmPassword')
+    .get( ()=> this._confirmPassword)
+    .set( value => this._confirmPassword = value )
+
+UserSchema.pre('validate', function(next) {
+    if (this.password !== this.confirmPassword) {
+        this.invalidate('confirmPassword', 'Password must match confirm password')
+    }
+    next()
+})
+
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+            this.password = hash
+            next()
+        })
 })
 
 const User = mongoose.model('User', userSchema)
