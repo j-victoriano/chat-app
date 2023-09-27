781a9a2fc3649f9ee9f55c513608f275f766c64d
Julian Victoriano <jvictoriano35@yahoo.com>
2023-09-26 21:45:39 -0700

    initial commit

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const dbName = 'chatappDb';
+
+mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(()=>console.log(`Connected to ${dbName} database!`))
+    .catch((err)=> console.log(err));
+
